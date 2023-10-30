7fb7bc0c881bec441a934a4444c9f620f2821f41
0hmori <mehowomori.fiofio.cute.38@gmail.com>
2023-10-30 19:56:49 +0900

    old('title', が消えてたので修正

--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -28,19 +28,18 @@
     <form action="{{ route('tasks.update', $task) }}" method="post">
         @csrf
         @method('PATCH')
-
         <p>
             <label for="title">論文タイトル</label><br>
-            <input type="text" name="title" id="title" value="{{ $task->title }}">
+            <input type="text" name="title" id="title" value="{{ old('title', $task->title) }}">
         </p>
         <p>
             <label for="body">本文</label><br>
-            <textarea name="body" class="body" id="body">{{ $task->body }}</textarea>
+            <textarea name="body" class="body" id="body">{{ old('body', $task->body) }}</textarea>
         </p>
+
         <input type="submit" value="更新">
     </form>
-    <button onclick='location.href="{{ route('tasks.show', $task) }}"'>詳細へ戻る</button>
-
+    <button onclick='location.href="{{ route('tasks.show', $task) }}"'>詳細に戻る</button>
 </body>
 
 </html>
